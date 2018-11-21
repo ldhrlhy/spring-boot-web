f93d50876180de563592951372532ffa97bfbfb3
dy <dy@dydeMacBook-Pro.lan>
2018-11-21 09:14:21 +0800

    swagger

--- src/main/java/com/xingchen/web/web/MessageController.java
+++ src/main/java/com/xingchen/web/web/MessageController.java
@@ -2,11 +2,16 @@ package com.xingchen.web.web;
 
 import com.xingchen.web.model.Message;
 import com.xingchen.web.repository.MessageRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "消息", description = "消息操作 API", position = 100, protocols = "http")
 @RestController
 @RequestMapping("/")
 public class MessageController {
@@ -14,6 +19,12 @@ public class MessageController {
     private MessageRepository messageRepository;
 
     // 获取所有消息体
+    @ApiOperation(
+            value = "消息列表",
+            notes = "完整的消息内容列表",
+            produces="application/json, application/xml",
+            consumes="application/json, application/xml",
+            response = List.class)
     @GetMapping(value = "messages")
     public List<Message> list() {
         List<Message> messages = this.messageRepository.findAll();
@@ -21,6 +32,12 @@ public class MessageController {
     }
 
     // 创建一个消息体
+    @ApiOperation(value = "添加消息", notes = "根据参数创建消息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "消息 ID", required = true, dataType = "Long", paramType = "query"),
+            @ApiImplicitParam(name = "text", value = "正文", required = true, dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "summary", value = "摘要", required = false, dataType = "String", paramType = "query"),
+    })
     @PostMapping(value = "message")
     public Message create(Message message) {
         message = this.messageRepository.save(message);
--- src/main/java/com/xingchen/web/web/WebController.java
+++ src/main/java/com/xingchen/web/web/WebController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/web")
 public class WebController {
     @RequestMapping(name="/getUser")
     public List<User> getUser() {
